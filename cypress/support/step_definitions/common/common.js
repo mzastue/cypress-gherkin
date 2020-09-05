32f1447e4efd55d6b12a34350f838e412dbb3eb5
Mateusz Littwin <mzastue@gmail.com>
2020-09-05 21:03:03 +0200

    Remove console.log

--- cypress/support/step_definitions/common/common.js
+++ cypress/support/step_definitions/common/common.js
@@ -3,7 +3,6 @@ import { Given } from "cypress-cucumber-preprocessor/steps";
 const baseUrl = Cypress.env("baseUrl");
 
 Given(`I am on {string}`, (url) => {
-  console.log(Cypress.env());
   cy.visit(`${baseUrl}${url}`);
 });
 
